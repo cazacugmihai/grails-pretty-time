ad23ff1ff102a47628708466d22a6731a8c955e9
Mihai Cazacu <cazacugmihai@gmail.com>
2009-06-20 22:10:46 +0000

    version 0.1

---
+++ PrettyTimeGrailsPlugin.groovy
@@ -0,0 +1,12 @@
+class PrettyTimeGrailsPlugin {
+
+    def version = '0.1'
+    def dependsOn = [:]
+
+    def author = 'Cazacu Mihai'
+    def authorEmail = 'cazacugmihai@yahoo.com'
+    def title = 'Pretty-time plugin'
+    def description = 'A plugin that allows you display human readable, relative timestamps.'
+    def documentation = 'http://grails.org/PrettyTime+Plugin'
+
+}
---
+++ grails-app/taglib/org/grails/prettytime/PrettyTimeTagLib.groovy
@@ -0,0 +1,71 @@
+package org.grails.prettytime
+
+import com.ocpsoft.pretty.time.PrettyTime
+import com.ocpsoft.pretty.time.units.Century
+import com.ocpsoft.pretty.time.units.Day
+import com.ocpsoft.pretty.time.units.Decade
+import com.ocpsoft.pretty.time.units.Hour
+import com.ocpsoft.pretty.time.units.JustNow
+import com.ocpsoft.pretty.time.units.Millennium
+import com.ocpsoft.pretty.time.units.Millisecond
+import com.ocpsoft.pretty.time.units.Minute
+import com.ocpsoft.pretty.time.units.Month
+import com.ocpsoft.pretty.time.units.Second
+import com.ocpsoft.pretty.time.units.Week
+import com.ocpsoft.pretty.time.units.Year
+
+class PrettyTimeTagLib {
+
+    static namespace = "prettytime"
+
+    def display = {attrs, body ->
+        def date = attrs.remove('date')
+        if (!date) throw new PrettyTimeException(
+                "There must be a 'date' attribute included in the prettytime tag.")
+
+        def prettyTime = new PrettyTime()
+        prettyTime.units = [
+                justNowUnitToI18n(new JustNow()),
+                unitToI18n(new Millisecond()),
+                unitToI18n(new Second()),
+                unitToI18n(new Minute()),
+                unitToI18n(new Hour()),
+                unitToI18n(new Day()),
+                unitToI18n(new Week()),
+                unitToI18n(new Month()),
+                unitToI18n(new Year()),
+                unitToI18n(new Decade()),
+                unitToI18n(new Century()),
+                unitToI18n(new Millennium())
+        ]
+
+        String result = prettyTime.format(date)
+        out << result
+    }
+
+    private def unitToI18n(unit) {
+        // pattern
+        unit.format.pattern = '%n %u '
+        // name/pluralName
+        def className = unit.class.name
+        className = className[className.lastIndexOf('.') + 1..-1].toLowerCase()
+        unit.name = g.message(code: "prettytime.$className")
+        unit.pluralName = g.message(code: "prettytime.${className}s")
+        // preffix/suffix
+        unit.format.pastPrefix = g.message(code: 'prettytime.past.prefix')
+        unit.format.pastSuffix = g.message(code: 'prettytime.past.suffix')
+        unit.format.futurePrefix = g.message(code: 'prettytime.future.prefix')
+        unit.format.futureSuffix = g.message(code: 'prettytime.future.suffix')
+        unit
+    }
+
+    private def justNowUnitToI18n(unit) {
+        // pattern
+        unit.format.pattern = '%u'
+        // preffix/suffix
+        unit.format.pastSuffix = g.message(code: 'prettytime.justnow.past.suffix')
+        unit.format.futureSuffix = g.message(code: 'prettytime.justnow.future.suffix')
+        unit
+    }
+
+}
