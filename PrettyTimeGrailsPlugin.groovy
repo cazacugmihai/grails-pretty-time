84609e59803bdba1ec8850628afa176c4b04575a
Mihai Cazacu <cazacugmihai@gmail.com>
2009-06-20 22:50:08 +0000

    Fixed some typos

--- PrettyTimeGrailsPlugin.groovy
+++ PrettyTimeGrailsPlugin.groovy
@@ -6,7 +6,7 @@ class PrettyTimeGrailsPlugin {
     def author = 'Cazacu Mihai'
     def authorEmail = 'cazacugmihai@yahoo.com'
     def title = 'Pretty-time plugin'
-    def description = 'A plugin that allows you display human readable, relative timestamps.'
+    def description = 'A plugin that allows you to display human readable, relative timestamps.'
     def documentation = 'http://grails.org/PrettyTime+Plugin'
 
 }
