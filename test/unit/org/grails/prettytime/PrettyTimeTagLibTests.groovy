c0944f627353638ccc059a42d74a1a54c0dad19a
Stefan Glase <stefan.glase@googlemail.com>
2013-08-02 20:46:08 +0200

    use better name for helper method

--- test/unit/org/grails/prettytime/PrettyTimeTagLibTests.groovy
+++ test/unit/org/grails/prettytime/PrettyTimeTagLibTests.groovy
@@ -5,58 +5,58 @@ import org.joda.time.Period
 
 class PrettyTimeTagLibTests {
 
-    String getBodyFromTemplate(def template, def params = []) {
+    String applyTemplateAndGetBody(def template, def params = []) {
         def tag = params ? applyTemplate(template, params) : applyTemplate(template)
         tag ? new XmlSlurper().parseText(tag).text() : ''
     }
 
     void testFormatDate() {
-        assert getBodyFromTemplate('<prettytime:display />') == ''
+        assert applyTemplateAndGetBody('<prettytime:display />') == ''
 
-        assert getBodyFromTemplate('<prettytime:display date="${new Date()}" />') == 'moments ago'
-        assert getBodyFromTemplate('<prettytime:display date="${new Date() - 7}" />') == '1 week ago'
-        assert getBodyFromTemplate('<prettytime:display date="${new Date() - 14}" />') == '2 weeks ago'
+        assert applyTemplateAndGetBody('<prettytime:display date="${new Date()}" />') == 'moments ago'
+        assert applyTemplateAndGetBody('<prettytime:display date="${new Date() - 7}" />') == '1 week ago'
+        assert applyTemplateAndGetBody('<prettytime:display date="${new Date() - 14}" />') == '2 weeks ago'
 
         def cal = Calendar.instance
         cal.add(Calendar.HOUR, -1)
-        assert getBodyFromTemplate('<prettytime:display date="${date}" />', [date: cal.time]) == '1 hour ago'
+        assert applyTemplateAndGetBody('<prettytime:display date="${date}" />', [date: cal.time]) == '1 hour ago'
 
         cal.add(Calendar.HOUR, 2)
-        assert getBodyFromTemplate('<prettytime:display date="${date}" />', [date: cal.time]) == '60 minutes from now'
+        assert applyTemplateAndGetBody('<prettytime:display date="${date}" />', [date: cal.time]) == '60 minutes from now'
     }
 
     void testFormatDateWIthJodaTime() {
         def dt = new DateTime()
-        assert getBodyFromTemplate('<prettytime:display date="${date}" />', [date: dt]) == 'moments ago'
+        assert applyTemplateAndGetBody('<prettytime:display date="${date}" />', [date: dt]) == 'moments ago'
 
         dt = dt.minus(Period.days(7))
-        assert getBodyFromTemplate('<prettytime:display date="${date}" />', [date: dt]) == '1 week ago'
+        assert applyTemplateAndGetBody('<prettytime:display date="${date}" />', [date: dt]) == '1 week ago'
 
         dt = dt.minus(Period.days(7))
-        assert getBodyFromTemplate('<prettytime:display date="${date}" />', [date: dt]) == '2 weeks ago'
+        assert applyTemplateAndGetBody('<prettytime:display date="${date}" />', [date: dt]) == '2 weeks ago'
     }
 
     void testFormatDateWithLong() {
         [new Date(), new Date() - 7, new Date() - 14].each { dateToTest ->
-            assert getBodyFromTemplate('<prettytime:display date="${date}" />', [date: dateToTest]) ==
-                    getBodyFromTemplate('<prettytime:display date="${date}" />', [date: dateToTest.getTime()])
+            assert applyTemplateAndGetBody('<prettytime:display date="${date}" />', [date: dateToTest]) ==
+                    applyTemplateAndGetBody('<prettytime:display date="${date}" />', [date: dateToTest.getTime()])
         }
     }
 
     void testWithDifferentLocale() {
-        assert getBodyFromTemplate('<prettytime:display date="${new Date()}" />') == 'moments ago'
+        assert applyTemplateAndGetBody('<prettytime:display date="${new Date()}" />') == 'moments ago'
 
         request.addPreferredLocale(Locale.GERMAN)
 
-        assert getBodyFromTemplate('<prettytime:display date="${new Date()}" />') == 'vor einem Augenblick'
-        assert getBodyFromTemplate('<prettytime:display date="${new Date() - 7}" />') == 'vor 1 Woche'
-        assert getBodyFromTemplate('<prettytime:display date="${new Date() - 14}" />') == 'vor 2 Wochen'
+        assert applyTemplateAndGetBody('<prettytime:display date="${new Date()}" />') == 'vor einem Augenblick'
+        assert applyTemplateAndGetBody('<prettytime:display date="${new Date() - 7}" />') == 'vor 1 Woche'
+        assert applyTemplateAndGetBody('<prettytime:display date="${new Date() - 14}" />') == 'vor 2 Wochen'
 
         request.addPreferredLocale(new Locale('pl'))
 
-        assert getBodyFromTemplate('<prettytime:display date="${new Date()}" />') == 'przed chwilą'
-        assert getBodyFromTemplate('<prettytime:display date="${new Date() - 7}" />') == '1 tydzień temu'
-        assert getBodyFromTemplate('<prettytime:display date="${new Date() - 14}" />') == '2 tygodni(e) temu'
+        assert applyTemplateAndGetBody('<prettytime:display date="${new Date()}" />') == 'przed chwilą'
+        assert applyTemplateAndGetBody('<prettytime:display date="${new Date() - 7}" />') == '1 tydzień temu'
+        assert applyTemplateAndGetBody('<prettytime:display date="${new Date() - 14}" />') == '2 tygodni(e) temu'
     }
 
 }
